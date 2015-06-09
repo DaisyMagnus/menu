8de6f8b8b94435269f339a25c78a11bfa36488a9
Daisy Magnus-Aryitey <daisymagnusaryitey@gmail.com>
2015-06-08 21:49:59 -0400

    Fix link returning class

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,9 +1,9 @@
 class Dish < ActiveRecord::Base
   belongs_to :course
 
-  # validates :price, :presence => true,
-  #           :numericality => true,
-  #           :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+  validates :price, :presence => true,
+            :numericality => true,
+            :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -28,4 +28,7 @@ Course.create!(name: "Dessert")
 
 Dish.create!(price:15.00, name:"Greek Salad", course_id:2, description: "A salad with feta, olives,
 and tomatoes.")
-Dish.create!(price:25.00, name:"Salmon", course_id:3, description: "A salmon on a bed of lettuce.")
+Dish.create!(price:10.00, name:"Truffled Filet of Beef Sandwiches", course_id:3, description: "A hearty but elegant sandwich.")
+Dish.create!(price:20.00, name:"Chicken with Goat Cheese and Basil", course_id:3, description: "A new take on a roast chicken.")
+Dish.create!(price:18.00, name:"Fresh Pea Soup", course_id:1, description: "A fresh and light soup.")
+Dish.create!(price:12.00, name:"Chocolate IceBox Cake", course_id:4, description: "Just like Ma used to make.")
